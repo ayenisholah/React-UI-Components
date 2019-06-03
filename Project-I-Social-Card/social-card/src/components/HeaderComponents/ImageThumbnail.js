2b327e0f809f52a5bc145600e696830653e3a8b4
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-03 13:00:28 +0100

    <ImageThumbnail /> created

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+function ImageThumbnail (props) {
+    return <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="Lambda Logo">{ props.text } ></img>
+}
+
+function App() {
+    return (
+        <div className="logo">
+            <ImageThumbnail text='test'/>
+        </div>
+    )
+}
+
+export default App;
