bdd159e6c960d97af2227066890bdad3ef957bd4
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-03 14:31:37 +0100

    All files imported

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -4,14 +4,22 @@ import ImageThumbnail from './components/HeaderComponents/ImageThumbnail'
 import HeaderContainer from './components/HeaderComponents/HeaderContainer'
 import HeaderTitle from './components/HeaderComponents/HeaderTitle'
 import HeaderContent from './components/HeaderComponents/HeaderContent'
+import CardContainer from './components/CardComponents/CardContainer'
+import CardBanner from './components/CardComponents/CardBanner'
+import CardContent from './components/CardComponents/CardContent'
+
 
 const App = () => {
   return (
     <div className="App">
+      
       <HeaderContainer />
       <HeaderTitle />
       <ImageThumbnail />
       <HeaderContent />
+      <CardContainer />
+      <CardBanner />
+      <CardContent />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './Card.css';
-import './CardBanner'
-import './CardContent'
 
 function CardContainer () {
     return <div>
