e5ea7dfd74de9762fa1101d0891dc795594831ff
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-03 13:16:28 +0100

    <HeaderTitle />

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 function HeaderContainer (props) {
     return <div>{ props.text } >
-
+        <p><strong>Lambda School Container</strong></p>
     </div>
 }
 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle () {
+    return (
+        <div> 
+            <h1>Lambda School</h1>
+            <p>@LambdaSchool</p>
+            <p>26 Jan</p>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <div className="header-title">
+            <HeaderTitle text='test'/>
+        </div>
+    )
+}
+
+export default App;
