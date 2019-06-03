0908be5ddcbcd79623f4eed00f7c4d1251789aa7
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-03 12:51:30 +0100

    <HeaderContainer /> created

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContainer (props) {
+    return <div>{ props.text } >
+
+    </div>
+}
+
+function App() {
+    return (
+        <div className="header-container">
+            <HeaderContainer text='test'/>
+        </div>
+    )
+}
+
+export default App;
