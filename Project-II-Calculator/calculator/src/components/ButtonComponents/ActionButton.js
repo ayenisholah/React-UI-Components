735e66183b782696aa04dc2f47317e9384107cab
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-04 14:02:32 +0100

    Button component created

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+function ActionButton () {
+    return (
+        <>
+        {
+
+        }
+        </>
+    )
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,29 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton (props) {
+    return (
+        <>
+            <div className="row">
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                
+            </div>
+            <div className="row">
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+            </div>
+            <div className="row">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+            </div>
+            <div className="row">
+                <button>0</button>
+            </div>
+        </>
+    )
+}
+export default NumberButton;
