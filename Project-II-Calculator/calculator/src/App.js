8db07347480b7661e8eb8834cccfc7967b2fc409
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-04 14:23:36 +0100

    Calculator displAY COMPONENT created

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton'
 import ActionButton from './components/ButtonComponents/ActionButton'
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay' 
+
 const App = () => {
   return (
     <>
       <h1>Calculator App</h1>
+      <CalculatorDisplay/>
       <ActionButton/>
       <NumberButton/>
     </>
