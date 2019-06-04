fc3a93a11676b6b0e0a35bc9942c73f73f015111
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-04 16:28:45 +0100

    .

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -4,26 +4,26 @@ import './Button.css';
 function NumberButton (props) {
     return (
         <>
-            <div className="digits">
+            <div className="row">
                 <button className={props.buttonStyle}>7{props.text}</button>
                 <button>8</button>
                 <button>9</button>
                 <button>X</button>
                 
             </div>
-            <div className="digits">
+            <div className="row">
                 <button>4</button>
                 <button>5</button>
                 <button>6</button>
                 <button>-</button>
             </div>
-            <div className="digits">
+            <div className="row">
                 <button>1</button>
                 <button>2</button>
                 <button>3</button>
                 <button>+</button>
             </div>
-            <div className="action">
+            <div className="row">
                 <button className='zero'>0</button>
                 <button>=</button>
             </div>
